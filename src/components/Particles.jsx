949726cb376aaa59e19843545d4b918f31543863
Riya922003 <riya98012@gmail.com>
2025-10-01 12:57:44 +0530

    new changes

--- src/components/Particles.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// This file was a legacy JS implementation of the Particles component.
-// The canonical, typed implementation lives in `Particles.tsx`.
-// Re-export the TSX component so existing imports continue to work.
-
-export { default } from './Particles';
